e1b7892ec5ad5dfb596fe5684c31b386aca15bcd
Matthew Day <Daystar.1998@yahoo.com>
2019-09-05 00:24:49 -0500

    Add main form

---
+++ Modeless Dialog Box/Modeless Dialog Box/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Modeless_Dialog_Box {
+	partial class mainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.changeColorButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// changeColorButton
+			// 
+			this.changeColorButton.Location = new System.Drawing.Point(189, 153);
+			this.changeColorButton.Name = "changeColorButton";
+			this.changeColorButton.Size = new System.Drawing.Size(121, 23);
+			this.changeColorButton.TabIndex = 0;
+			this.changeColorButton.Text = "Change Color";
+			this.changeColorButton.UseVisualStyleBackColor = true;
+			// 
+			// mainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(507, 344);
+			this.Controls.Add(this.changeColorButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "mainForm";
+			this.Text = "Change Colors";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button changeColorButton;
+	}
+}
+

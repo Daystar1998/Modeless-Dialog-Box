16acd3748fd3c85f8c8877e1df3391f72001a5df
Matthew Day <Daystar.1998@yahoo.com>
2019-09-05 00:34:14 -0500

    Add buttons to ChooseColorForm

--- Modeless Dialog Box/Modeless Dialog Box/ChooseColorForm.Designer.cs
+++ Modeless Dialog Box/Modeless Dialog Box/ChooseColorForm.Designer.cs
@@ -23,21 +23,77 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.redRadioButton = new System.Windows.Forms.RadioButton();
+			this.greenRadioButton = new System.Windows.Forms.RadioButton();
+			this.blueRadioButton = new System.Windows.Forms.RadioButton();
+			this.okButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// redRadioButton
+			// 
+			this.redRadioButton.AutoSize = true;
+			this.redRadioButton.Location = new System.Drawing.Point(12, 19);
+			this.redRadioButton.Name = "redRadioButton";
+			this.redRadioButton.Size = new System.Drawing.Size(45, 17);
+			this.redRadioButton.TabIndex = 0;
+			this.redRadioButton.TabStop = true;
+			this.redRadioButton.Text = "Red";
+			this.redRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// greenRadioButton
+			// 
+			this.greenRadioButton.AutoSize = true;
+			this.greenRadioButton.Location = new System.Drawing.Point(12, 42);
+			this.greenRadioButton.Name = "greenRadioButton";
+			this.greenRadioButton.Size = new System.Drawing.Size(54, 17);
+			this.greenRadioButton.TabIndex = 1;
+			this.greenRadioButton.TabStop = true;
+			this.greenRadioButton.Text = "Green";
+			this.greenRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// blueRadioButton
+			// 
+			this.blueRadioButton.AutoSize = true;
+			this.blueRadioButton.Location = new System.Drawing.Point(12, 65);
+			this.blueRadioButton.Name = "blueRadioButton";
+			this.blueRadioButton.Size = new System.Drawing.Size(46, 17);
+			this.blueRadioButton.TabIndex = 2;
+			this.blueRadioButton.TabStop = true;
+			this.blueRadioButton.Text = "Blue";
+			this.blueRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// okButton
+			// 
+			this.okButton.Location = new System.Drawing.Point(63, 115);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(75, 23);
+			this.okButton.TabIndex = 3;
+			this.okButton.Text = "OK";
+			this.okButton.UseVisualStyleBackColor = true;
 			// 
 			// ChooseColorForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(270, 185);
+			this.ClientSize = new System.Drawing.Size(201, 150);
+			this.Controls.Add(this.okButton);
+			this.Controls.Add(this.blueRadioButton);
+			this.Controls.Add(this.greenRadioButton);
+			this.Controls.Add(this.redRadioButton);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.Name = "ChooseColorForm";
 			this.Text = "Choose";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RadioButton redRadioButton;
+		private System.Windows.Forms.RadioButton greenRadioButton;
+		private System.Windows.Forms.RadioButton blueRadioButton;
+		private System.Windows.Forms.Button okButton;
 	}
 }
